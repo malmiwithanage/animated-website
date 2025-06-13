06a566484c39f21221453fe645158a81fafd89d0
malmiwithanage <umnew1@gmail.com>
2025-06-13 11:57:08 +0530

    update App component styles and refine Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-pink-500 font-bold text-center mt-20'>
+      <h1 className='text-5xl text-violet-300 font-bold text-center mt-20 font-robert-medium'>
         Welcome
       </h1>
     </main>
