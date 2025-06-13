7798e1cf07be17b6973a15456e87555e82e19f7d
malmiwithanage <umnew1@gmail.com>
2025-06-13 13:17:22 +0530

    feat: implement video handling and interaction in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import {useRef, useState} from 'react';
 
 const Hero = () => {
+    const [currentIndex, setCurrentIndex] = useState(1);
+
+    const [hasClicked, setHasClicked] = useState(false);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [loadedVideos, setLoadedVideos] = useState(0);
+
+    const totalVideos = 3;
+
+    const nextVideoRef = useRef(null);
+
+    const handleMiniVdClick = () => {
+        setHasClicked(true);
+
+        setCurrentIndex(upcomingVideoIndex);
+    }
+
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
+    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+    const handleVideoLoaded = () => {}
+
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden '>
       <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
         <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div>
-                MinivideoPlayer
+            <div onClick={handleMiniVdClick} className='origin-cente scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
+                
+                <video ref={nextVideoRef}
+                src={getVideoSrc(currentIndex + 1 )}
+                loop
+                muted
+                id="current-video"
+                className='size-64 origin-center scale-150 object-cover object-center'
+                onLoadedData={handleVideoLoaded}
+                />
             </div>
         </div>
       </div>
