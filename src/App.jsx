395e7b611d912d36d02a7d5d770fc8a2de9a074f
malmiwithanage <umnew1@gmail.com>
2025-06-13 11:07:48 +0530

    initialize React project with Vite, Tailwind CSS, and ESLint setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-pink-500 font-bold text-center mt-20'>
+        Welcome
+      </h1>
+    </main>
+  )
+}
+
+export default App
