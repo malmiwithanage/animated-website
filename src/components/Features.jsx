0fccd5060629902f7e6bccfdcead3a52c40fcb3f
malmiwithanage <umnew1@gmail.com>
2025-06-17 16:43:45 +0530

    feat: refactor BentoCard component to include video background and improve layout structure

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 const BentoCard = ({src, title, description, isComingSoon}) => {
   return ( 
-    <div>
-        {title && (
-            <h3>{title}</h3>
-        )}
+    <div className='relative size-full'>
+        <video
+            src={src}
+            loop
+            muted
+            autoPlay
+            className='absolute left-0 top-0 size-full object-cover object-center'
+        />
+
+        <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
+
+        <div>
+            <h1>{title}</h1>
+        </div>
+        </div>
+
+        
+        
     </div>
    )
 }
