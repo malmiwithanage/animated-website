4ada61eee70478c8c860feaec516f5ebe440bc9c
malmiwithanage <umnew1@gmail.com>
2025-06-17 15:14:04 +0530

    feat: add background color to main layout and refactor NavBar component structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+import Button from "./Button";
+
+const navItems = ["Nexus", "Vault", "Prologue", "ABout", "Contact"];
 
 const NavBar = () => {
+  const navContainerRef = useRef(null);
   return (
-    <div>
-    NavBar 
-    </div>
-  )
-}
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="img/logo.png" alt="logo" className="w-10" />
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
 
-export default NavBar
+          <div className="flex- h-full items-center ">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a>
+
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
