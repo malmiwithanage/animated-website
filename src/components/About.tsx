dfa3807abf15aec1e2335e287597c0d70d5ef0ce
malmiwithanage <umnew1@gmail.com>
2025-06-17 10:01:30 +0530

    feat: add AnimatedTitle component for scroll-triggered animations and enhance About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger } from 'gsap/all'
+import AnimatedTitle from './AnimatedTitle'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -23,32 +24,33 @@ const About = () => {
             width: '100vw',
             height: '100vh',
             borderRadius: '0',
-
-    })
+        }) // Added missing closing parenthesis
     })
 
   return (
     <div id="about" className='min-h-screen w-screen'>
       <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
         <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
-        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-            Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure
-        </div>
+
+        <AnimatedTitle 
+          title="Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure" 
+          containerClass="mt-5 text-black text-center"
+        />
+
         <div className='about-subtext'>
-            <p>The Game of Games negins-your life, now an epic MMORPG</p>
+            <p>The Game of Games begins - your life, now an epic MMORPG</p> {/* Fixed typo: "negins" to "begins" */}
             <p>Zentry unites every player from countless games and platforms</p>
         </div>
 
       </div>
-      <div className='h-dvh w-screen' id="clip">
-        <div className='mask-clip-path about-image'>
-            <img
-                src="img/about.webp"
-                alt="Background"
-                className="absolute left-0 top-0 size-full object-cover"
-            />
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
         </div>
-
       </div>
     </div>
   )
