dfd41802f95325d0fccd9dc062c8c042f6849430
malmiwithanage <umnew1@gmail.com>
2025-06-22 12:07:23 +0530

    feat: enhance BentoCard component with styled title and conditional description rendering

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,7 +14,11 @@ const BentoCard = ({src, title, description, isComingSoon}) => {
         <div className='relative z-10 flex size-full flex-col justify-between p-5 text-blue-50'>
 
         <div>
-            <h1>{title}</h1>
+            <h1 className='bento-title special-font'>{title}</h1>
+            {description && (
+                <p className='mt-3 max-w-64 text-xs md:text-base'>
+                    {description}
+                </p>)}
         </div>
         </div>
 
