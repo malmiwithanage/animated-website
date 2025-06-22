05014fbc3a1dc4d039183016090d3c46efe8948a
malmiwithanage <umnew1@gmail.com>
2025-06-22 13:15:22 +0530

    feat: add Footer component with social links and privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Features from "./components/Features";
+import Story from "./components/Story";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <About/>
       <Features/>
+      <Story/>
+      <Footer/>
     </main>
   );
 };
